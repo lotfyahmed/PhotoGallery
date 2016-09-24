7402b077380d67092d6153a7dd65f71887f4db55
Ahmed Lotfy <ahmed.lotfy@badrit.com>
2016-09-24 08:27:25 +0200

    Take Photo from camera

--- PhotoGallery/PhotoCollectionViewController.swift
+++ PhotoGallery/PhotoCollectionViewController.swift
@@ -14,11 +14,11 @@ private let addImageIcon = "addImage"
 class PhotoCollectionViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIActionSheetDelegate {
     
     private var carsImages:[UIImage?] = [UIImage(named:"car 1"),
-                                       UIImage(named:"car 2"),
-                                        UIImage(named:"car 3"),
-                                            UIImage(named:"car 4"),
-                                                UIImage(named:"car 5"),
-                                                    UIImage(named:"car 6")]
+                                         UIImage(named:"car 2"),
+                                         UIImage(named:"car 3"),
+                                         UIImage(named:"car 4"),
+                                         UIImage(named:"car 5"),
+                                         UIImage(named:"car 6")]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Register cell classes
@@ -63,9 +63,7 @@ class PhotoCollectionViewController: UICollectionViewController, UIImagePickerCo
         }
     }
     
-    func chooePhotoSheet()
-    {
-        
+    func chooePhotoSheet(){
         let actionSheetController: UIAlertController = UIAlertController(title: "Please select Photo", message: "", preferredStyle: .ActionSheet)
         
         let cancelActionButton: UIAlertAction = UIAlertAction(title: "Cancel", style: .Cancel) { action -> Void in
@@ -76,29 +74,44 @@ class PhotoCollectionViewController: UICollectionViewController, UIImagePickerCo
         let saveActionButton: UIAlertAction = UIAlertAction(title: "Camera", style: .Default)
         { action -> Void in
             print("Take Image by Camera")
+            self.showImagePicker(.Camera)
         }
         actionSheetController.addAction(saveActionButton)
         
         let deleteActionButton: UIAlertAction = UIAlertAction(title: "Library", style: .Default)
         { action -> Void in
             print("Take Image From Gallery")
-            self.showImagePicker()
+            self.showImagePicker(.Library)
         }
         actionSheetController.addAction(deleteActionButton)
         self.presentViewController(actionSheetController, animated: true, completion: nil)
     }
     
+    enum PhotoSource {
+        case Camera
+        case Library
+    }
     
-    func showImagePicker(){
+    func showImagePicker(photoSource:PhotoSource){
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
         
-        guard UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) else{
-            return
+        switch photoSource {
+        case .Camera:
+            guard UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) else{
+                print("There isnot Camera")
+                return
+            }
+            imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
+        case .Library:
+            guard UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) else{
+                 print("There isnot Gallery")
+                return
+            }
+            imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
         }
-            let imagePicker = UIImagePickerController()
-            imagePicker.delegate = self
-            imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary;
-            imagePicker.allowsEditing = false
-            self.presentViewController(imagePicker, animated: true, completion: nil)
+        imagePicker.allowsEditing = false
+        self.presentViewController(imagePicker, animated: true, completion: nil)
     }
     
     
