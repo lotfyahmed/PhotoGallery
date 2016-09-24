df876a78a95cc517a4ff43a93fa4a22aab7bd5b9
Ahmed Lotfy <ahmed.lotfy@badrit.com>
2016-09-24 07:46:55 +0200

    Add Image Action

--- PhotoGallery/PhotoCollectionViewController.swift
+++ PhotoGallery/PhotoCollectionViewController.swift
@@ -11,9 +11,15 @@ import UIKit
 private let reuseIdentifier = "CarImageCell"
 private let addImageIcon = "addImage"
 
-class PhotoCollectionViewController: UICollectionViewController {
+class PhotoCollectionViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    var carsImages:[String] = ["car 1", "car 2", "car 3", "car 4", "car 5", "car 6", "car 1", "car 2", "car 3"]
+    private let imagePicker = UIImagePickerController()
+    private var carsImages:[UIImage?] = [UIImage(named:"car 1"),
+                                       UIImage(named:"car 2"),
+                                        UIImage(named:"car 3"),
+                                            UIImage(named:"car 4"),
+                                                UIImage(named:"car 5"),
+                                                    UIImage(named:"car 6")]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Register cell classes
@@ -37,7 +43,7 @@ class PhotoCollectionViewController: UICollectionViewController {
         if indexPath.row == carsImages.count {
             cell.carImage.image = UIImage(named:addImageIcon)
         }else{
-            cell.carImage.image = UIImage(named:carsImages[indexPath.row])
+            cell.carImage.image = carsImages[indexPath.row]
         }
         return cell
     }
@@ -46,5 +52,33 @@ class PhotoCollectionViewController: UICollectionViewController {
     {
         let length = (UIScreen.mainScreen().bounds.width-15)/2
         return CGSizeMake(length,length);
+    }
+    
+    override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath){
+        if indexPath.row == carsImages.count {
+            showImagePicker()
+        }
+    }
+    
+    func showImagePicker(){
+        
+        guard UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) else{
+            return
+        }
+            print("Button capture")
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary;
+            imagePicker.allowsEditing = false
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: NSDictionary!){
+        self.dismissViewControllerAnimated(true, completion: { () -> Void in
+            
+        })
+        carsImages.append(image)
+        self.collectionView?.reloadData()
     }
 }
